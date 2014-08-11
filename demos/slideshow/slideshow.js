d41c0206d0784d13aef98d29eea74c03d0139721
Josh Marinacci <joshua@marinacci.org>
2014-08-10 21:21:29 -0700

    updated demo

--- demos/slideshow/slideshow.js
+++ demos/slideshow/slideshow.js
@@ -9,6 +9,13 @@ if(process.argv.length < 3) {
     return;
 }
 
+var dir = process.argv[2];
+var filelist = fs.readdirSync(dir).map(function(file) {
+    return dir+'/'+file;
+});
+
+
+
 function CircularBuffer(arr) {
     this.arr = arr;
     this.index = -1;
@@ -18,19 +25,16 @@ function CircularBuffer(arr) {
     }
 }
 
+//wrap files in a circular buffer
+var files = new CircularBuffer(filelist);
+
+
 amino.start(function(core, stage) {
     stage.setSize(800,600);
 
     var root = new Group();
     stage.setRoot(root);
 
-    var dir = process.argv[2];
-    var files = fs.readdirSync(dir).map(function(file) {
-        return dir+'/'+file;
-    });
-
-    //wrap files in a circular buffer
-    var files = new CircularBuffer(files);
 
     var sw = stage.getW();
     var sh = stage.getH();
@@ -38,6 +42,9 @@ amino.start(function(core, stage) {
     //create two image views
     var iv1 = new ImageView().x(0);
     var iv2 = new ImageView().x(1000);
+
+    //add to the scene
+    root.add(iv1,iv2);
 
     //auto scale them
     function scaleImage(img,prop,obj) {
@@ -51,27 +58,24 @@ amino.start(function(core, stage) {
     iv1.src(files.next());
     iv2.src(files.next());
 
-    //add to the scene
-    root.add(iv1,iv2);
 
 
     //animate out and in
     function swap() {
         iv1.x.anim().delay(1000).from(0).to(-sw).dur(3000).start();
         iv2.x.anim().delay(1000).from(sw).to(0).dur(3000)
-        .then(function() {
-            //swap images and move views back in place
-            iv1.x(0);
-            iv2.x(sw);
-            iv1.image(iv2.image());
-            iv2.src(files.next());
-            //recurse
-            swap();
-        })
-        .start();
+            .then(afterAnim).start();
     }
-
     swap();
 
-    //TODO: make image loading asynchronous, and work over the network
+    function afterAnim() {
+        //swap images and move views back in place
+        iv1.x(0);
+        iv2.x(sw);
+        iv1.image(iv2.image());
+        iv2.src(files.next());
+        //recurse
+        swap();
+    }
+
 });
